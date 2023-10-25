c800c1a5aba55ae4d8a0a56232a5c318bd224bfa
Sebastian Burgos <sburgos1981@gmail.com>
2023-10-24 23:27:26 -0300

    uno al master

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,4 +1,1 @@
-document.getElementById('formulario-contacto').addEventListener('submit', function (event) {
-    // Lógica de validación aquí
-    event.preventDefault(); // Evitar que se envíe el formulario si la validación falla
-});
+
