9ac56ce86583ed012ff5927f36ed41e6d4064fcb
Sebastian Burgos <sburgos1981@gmail.com>
2023-10-26 09:17:06 -0300

    Unifico rama Noe

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,1 +1,25 @@
+ //Archivo: js/validacion.js
+ document.addEventListener('DOMContentLoaded', function () {
+    const formulario = document.querySelector('.formulario');
+
+    formulario.addEventListener('submit', function (event) {
+        // Evitar que el formulario se envíe automáticamente
+        event.preventDefault();
+
+        // Obtener los valores de los campos
+        const nombre = document.getElementById('nombre').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const telefono = document.getElementById('telefono').value.trim();
+        const mensaje = document.getElementById('mensaje').value.trim();
+
+        // Validar que los campos no estén vacíos
+        if (nombre === '' || email === '' || telefono === '' || mensaje === '') {
+            alert('Por favor, complete todos los campos del formulario.');
+        } else {
+            // Aquí podrías agregar lógica adicional o enviar el formulario
+            alert('Formulario enviado correctamente.');
+             formulario.submit();  // Descomenta esta línea para enviar el formulario realmente
+        }
+    });
+});
 
